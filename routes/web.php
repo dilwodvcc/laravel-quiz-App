823ea68ee0ca17b7fb9ec636ead8a36f8e8f75ec
dilwodvc <abdullokh207@example.com>
2025-02-07 10:20:09 +0500

    complate about, home, register, login, how-it-works view

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,21 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
+Route::get('/welcome', function () {
     return view('welcome');
+});
+Route::get('/', function () {
+    return view('home');
+});
+Route::get('/login', function () {
+    return view('auth/login');
+});
+Route::get('/register', function () {
+    return view('auth/register');
+});
+Route::get('/about', function () {
+    return view('about');
+});
+Route::get('/take-quiz', function () {
+    return view('quiz/take-quiz');
 });
